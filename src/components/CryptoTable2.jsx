c1e24b071f0c3069cfee0db02192697b2c9fd20e
Lyt Code <rdilshad3559@gmail.com>
2024-01-28 10:23:39 +0530

    cryptoTable 2

---
+++ src/components/CryptoTable2.jsx
@@ -0,0 +1,132 @@
+import React from "react";
+
+import { data } from "@/constants/change_in_crypto";
+
+const CryptoTable2 = ({ stats }) => {
+  return (
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg max-w-[85%]  z-999">
+      <table className="w-full text-sm text-left rtl:text-right   text-white  font-semibold bg-[#1E2A31]  ">
+        <thead className="text-xs   uppercase bg-gray-700    text-white  font-semibold">
+          <tr>
+            <th scope="col" className="p-4">
+              #
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Price
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Market Cap
+            </th>
+            <th scope="col" className="px-6 py-3">
+              24h Volume
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Day
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Week
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Month
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Year
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Circulating Supply
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {stats?.map((item, index) => (
+            <tr className="   border-gray-700   hover:bg-gray-600" key={index}>
+              <td className="w-4 p-4">{index + 1}</td>
+              <th
+                scope="row"
+                className="px-6 py-4   whitespace-nowrap   text-white  font-semibold"
+              >
+                {item.coin_name}
+              </th>
+              <td className="px-6 py-4">{item?.crypto_stats?.Open}$</td>
+              <td className="px-6 py-4">
+                {item?.crypto_stats?.["Market Cap"]}$
+              </td>
+              <td className="px-6 py-4">{item?.crypto_stats?.Volume}$</td>
+              {data?.map((i, ind) => {
+                if (i.coin_name === item.coin_name) {
+                  return (
+                    <td
+                      className={`px-6 py-4 border-b ${
+                        i.change_day >= 0 == 0 ? "bg-red-500" : "bg-green-500"
+                      }`}
+                      key={ind}
+                    >
+                      {i.change_day.toFixed(2)}%
+                    </td>
+                  );
+                } else {
+                  return null;
+                }
+              })}
+              {data?.map((i, ind) => {
+                if (i.coin_name === item.coin_name) {
+                  return (
+                    <td
+                      className={`px-6 py-4 border-b ${
+                        i.change_week >= 0 == 0 ? "bg-red-500" : "bg-green-500"
+                      }`}
+                      key={ind}
+                    >
+                      {i.change_week.toFixed(2)}%
+                    </td>
+                  );
+                } else {
+                  return null;
+                }
+              })}
+              {data?.map((i, ind) => {
+                if (i.coin_name === item.coin_name) {
+                  return (
+                    <td
+                      className={`px-6 py-4 border-b ${
+                        i.change_month >= 0 == 0 ? "bg-red-500" : "bg-green-500"
+                      }`}
+                      key={ind}
+                    >
+                      {i.change_month.toFixed(2)}%
+                    </td>
+                  );
+                } else {
+                  return null;
+                }
+              })}
+              {data?.map((i, ind) => {
+                if (i.coin_name === item.coin_name) {
+                  return (
+                    <td
+                      className={`px-6 py-4 border-b ${
+                        i.change_year >= 0 == 0 ? "bg-red-500" : "bg-green-500"
+                      }`}
+                      key={ind}
+                    >
+                      {i.change_year.toFixed(2)}%
+                    </td>
+                  );
+                } else {
+                  return null;
+                }
+              })}
+              <td className="px-6 py-4">
+                {item?.crypto_stats?.["Circulating Supply"]}$
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+export default CryptoTable2;
--- src/pages/compare.js
+++ src/pages/compare.js
@@ -1,66 +1,64 @@
 import React, { useState } from "react";
 import { X } from "lucide-react";
 import SelectCryptoForCompare from "@/components/SelectCryptoForCompare";
-import CryptoTable from "@/components/CryptoTable";
+import CryptoTable from "@/components/CryptoTable2";
 import CompareChart from "@/components/compare-page/CompareCharts";
 
 import { stats } from "@/constants/crypto_stats";
 
 const Page = () => {
-    const [selectedCoins, setSelectedCoins] = useState([]);
+  const [selectedCoins, setSelectedCoins] = useState([]);
 
-    const stats1 = stats.filter((coin) => {
-        if (selectedCoins.includes(coin.coin_name)) {
-            return coin;
-        }
-    });
+  const stats1 = stats.filter((coin) => {
+    if (selectedCoins.includes(coin.coin_name)) {
+      return coin;
+    }
+  });
 
-    return (
-        <section className=" grid grid-cols-7 w-screen my-2">
-            <div className="col-span-6 my-2">
-                {selectedCoins.length && (
-                    <div className=" justify-start  ">
-                        {<CryptoTable stats={stats1} />}
-                    </div>
-                )}
+  return (
+    <section className=" grid grid-cols-7 w-screen my-2">
+      <div className="col-span-6 my-2">
+        {selectedCoins.length && (
+          <div className=" justify-start  ">
+            {<CryptoTable stats={stats1} />}
+          </div>
+        )}
 
-                <div className="max-w-[85%] my-2">
-                    {selectedCoins.map((coin, index) => (
-                        <div key={index} className="rounded-md">
-                            <CompareChart bubble={coin} />
-                        </div>
-                    ))}
-                </div>
+        <div className="max-w-[85%] my-2">
+          {selectedCoins.map((coin, index) => (
+            <div key={index} className="rounded-md">
+              <CompareChart bubble={coin} />
             </div>
-            <div className="col-span-1  min-h-[90vh] rounded-md p-1 bg-gray-700">
-                <div className="py-2">
-                    <SelectCryptoForCompare
-                        selectedCoins={selectedCoins}
-                        setSelectedCoins={setSelectedCoins}
-                    />
-                </div>
-                <div className="grid grid-cols-1 text-gray-200 py-5  ">
-                    {selectedCoins.map((coin, index) => (
-                        <p
-                            key={index}
-                            className="col-span-1 pl-5 pr-7 font-semibold flex justify-between items-center"
-                        >
-                            <span>{coin}</span>
-                            <X
-                                className="mr-2 m-1"
-                                size={15}
-                                onClick={() => {
-                                    setSelectedCoins(
-                                        selectedCoins.filter((c) => c !== coin)
-                                    );
-                                }}
-                            />
-                        </p>
-                    ))}
-                </div>
-            </div>
-        </section>
-    );
+          ))}
+        </div>
+      </div>
+      <div className="col-span-1  min-h-[90vh] rounded-md p-1 bg-gray-700">
+        <div className="py-2">
+          <SelectCryptoForCompare
+            selectedCoins={selectedCoins}
+            setSelectedCoins={setSelectedCoins}
+          />
+        </div>
+        <div className="grid grid-cols-1 text-gray-200 py-5  ">
+          {selectedCoins.map((coin, index) => (
+            <p
+              key={index}
+              className="col-span-1 pl-5 pr-7 font-semibold flex justify-between items-center"
+            >
+              <span>{coin}</span>
+              <X
+                className="mr-2 m-1"
+                size={15}
+                onClick={() => {
+                  setSelectedCoins(selectedCoins.filter((c) => c !== coin));
+                }}
+              />
+            </p>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Page;
